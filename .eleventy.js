3f45236ed54ab4bf34c827090f280efc3031d83b
flosincapite <flosincapite@gmail.com>
2022-10-23 18:27:28 -0500

    Add alien favicon gif and snail gif!

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,28 @@
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes) {
+  const metadata = await Image(
+    src,
+    {
+      widths: [300, 600],
+      formats: ["gif", "avif"]
+    }
+  );
+
+  const imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async"
+  };
+
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+  eleventyConfig.addPassthroughCopy("./_src/css/");
+  eleventyConfig.addPassthroughCopy("./_src/images/");
   return {
     dir: {
       input: '_src',
